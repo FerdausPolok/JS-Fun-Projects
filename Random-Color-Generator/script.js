30972eccf333807f210a426efcf2909bee0976cf
Ferdaus Zaman Polok <ferdauszamanpolok@Ferdauss-Mac-mini.local>
2022-11-15 20:10:59 +0600

    Comment updated

--- Random-Color-Generator/script.js
+++ Random-Color-Generator/script.js
@@ -1,4 +1,4 @@
-function getNewColor() {  //creating random color + seeting it to bg
+function getNewColor() {  //function for generating random color code and setting them into the UI
   const SYMBOLS = "0123456789ABCDEF";
   let color = "#";
   const MAGIC_NUMBER = 16
@@ -6,7 +6,7 @@ function getNewColor() {  //creating random color + seeting it to bg
     //creating random color code
     color += SYMBOLS[Math.floor(Math.random() * MAGIC_NUMBER)];
   }
-  document.body.style.background = color; //setting the random color code on the background
+  document.body.style.background = color; //setting the random color code into the background
 
   document.getElementById("hex").innerHTML = color; //showing current color code on UI
 }
