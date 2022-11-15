4711b54c8f31348fbdc689a8474703804ddf68a6
Ferdaus Zaman Polok <ferdauszamanpolok@Ferdauss-Mac-mini.local>
2022-11-15 19:47:30 +0600

    Bug fixed + reorganized js code

--- Random-Color-Generator/script.js
+++ Random-Color-Generator/script.js
@@ -1,10 +1,13 @@
-function getNewColor(){  //creating random color + seeting it to bg
-    let symbols, color;
-    symbols= "0123456789ABCDEF";
-    color= '#'
-    for(var i=0; i<6; i++){ //creating random color code
-        color+= symbols[Math.floor(Math.random() * 16)]
-    }
-    console.log(color)
-    document.body.style.background = color; //setting the random color code
+function getNewColor() {  //creating random color + seeting it to bg
+  const symbols = "0123456789ABCDEF";
+  let color = "#";
+  const magic_number = 16
+  for (var i = 0; i < 6; i++) {
+    //creating random color code
+    color += symbols[Math.floor(Math.random() * magic_number)];
+  }
+  console.log(color);
+  document.body.style.background = color; //setting the random color code on the background
+
+  document.getElementById("hex").innerHTML = color; //showing current color code on UI
 }
