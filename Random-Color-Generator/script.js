d11346ef932bb64c3765caeb32c453bdb4245811
Ferdaus Zaman Polok <ferdauszamanpolok@Ferdauss-Mac-mini.local>
2022-11-15 19:57:37 +0600

    Converted lowercase const variable declaration to uppercase as per the convention

--- Random-Color-Generator/script.js
+++ Random-Color-Generator/script.js
@@ -1,10 +1,10 @@
 function getNewColor() {  //creating random color + seeting it to bg
-  const symbols = "0123456789ABCDEF";
+  const SYMBOLS = "0123456789ABCDEF";
   let color = "#";
-  const magic_number = 16
+  const MAGIC_NUMBER = 16
   for (var i = 0; i < 6; i++) {
     //creating random color code
-    color += symbols[Math.floor(Math.random() * magic_number)];
+    color += SYMBOLS[Math.floor(Math.random() * MAGIC_NUMBER)];
   }
   console.log(color);
   document.body.style.background = color; //setting the random color code on the background
