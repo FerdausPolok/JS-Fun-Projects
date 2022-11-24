674152245a7b52ed69f44cba80fd2b6afedb8927
Ferdaus Zaman Polok <ferdauszamanpolok@Ferdauss-Mac-mini.local>
2022-11-24 16:28:18 +0600

    Github finder: Implemented the profile info finding part showing part

---
+++ Github-Finder/JS/profile_ui.js
@@ -0,0 +1,80 @@
+class UI {
+  constructor() {
+    //to save the profile div here
+    this.profile = document.querySelector("#profile");
+  }
+
+  showProfile(user) {
+    this.clearAlert();
+
+    this.profile.innerHTML = `
+        <div class="card card-body mb-3 ">
+        <div class="row">
+          <div class="col-md-3">
+            <img class="img-fluid mb-2" src="${user.avatar_url}">
+            <p target="_blank" class="btn btn-dark btn-block mb-1" style= "border-radius: 4px;" >${user.name}</p>
+
+            <p class="text-center list-group-item mb-1" style= "font-size: 14px; font-family: sans-serif; " >${user.bio} </p>
+
+            <a href="${user.html_url}" target="_blank" class="btn btn-primary btn-block mb-1" style= "border-radius: 4px;" >View Profile</a>
+
+          </div>
+          <div class="col-md-9">
+            <span class="badge badge-primary">Public Repos: ${user.public_repos}</span>
+            <span class="badge badge-success">Followers: ${user.followers}</span>
+            <span class="badge badge-info">Following: ${user.following}</span>
+            <br><br>
+            <ul class="list-group">
+            <li class="list-group-item"> <b> Username: </b> ${user.login} </li>
+            <li class="list-group-item"> <b> ID: </b> ${user.id} </li>
+            <li class="list-group-item"> <b> Email: </b> ${user.email} </li>
+            <li class="list-group-item"> <b> Company: </b> ${user.company} </li>
+            <li class="list-group-item"> <b> Website/Blog: </b> ${user.blog} </li>
+            <li class="list-group-item"> <b> Location: </b> ${user.location} </li>
+            <li class="list-group-item"> <b> Member Since: </b> ${user.created_at.slice(0, 7)} </li> 
+            <li class="list-group-item"> <b> Last Contribution: </b> ${user.updated_at.slice(0, 7)} </li> 
+            </ul>
+          </div>
+        </div>
+      </div>
+
+
+      <div class="card card-body mb-3 ">
+        <div class="col-md-12">
+        <p target="_blank" class="btn btn-dark btn-block mb-1" style= "border-radius: 4px;" >Public Repositories</p>
+
+          <ul class="list-group">
+            <li class="list-group-item"> <b> Will Implement Soon! </b></li>
+          </ul>
+        
+          </div>
+      </div>
+    </div>
+        `;
+  }
+
+  showRepo(user) { }
+
+  clearProfile() {
+    this.profile.innerHTML = "";
+  }
+
+  showAlert(message, className) {
+    this.clearAlert();
+    this.clearProfile();
+    let div = document.createElement("div");
+    div.className = className;
+    div.appendChild(document.createTextNode(message));
+    let container = document.querySelector(".searchContainer");
+    let search = document.querySelector(".search");
+    container.insertBefore(div, search);
+  }
+
+  clearAlert() {
+    //for sake on not getting list of alerts
+    let currentAlert = document.querySelector(".alert");
+    if (currentAlert) {
+      currentAlert.remove();
+    }
+  }
+}
---
+++ Github-Finder/JS/script.js
@@ -0,0 +1,43 @@
+let searchBtn = document.querySelector("#searchBtn"); //search button
+let searchUser = document.querySelector("#searchUser"); //user input form data from search bar
+let ui = new UI();
+
+searchBtn.addEventListener("click", (e) => {
+    let userGivenText = searchUser.value;
+    if (userGivenText != "") {
+        //Geting User Data via Fetch API
+        fetch(`https://api.github.com/users/${userGivenText}`)
+            .then(result => result.json())
+            .then(data => {
+                //Geting User Repo Data via Fetch API   
+                fetch(data.repos_url)
+                    .then(result => result.json())
+                    .then(data2 => {
+                        console.log(data2);
+                        // ui.showRepo(data2)
+                        // if(data2.message== 'Not Found'){
+                        //     //Show Alert that usernamen not found
+                        //     ui.showAlert("User not found!", "alert alert-danger")
+                        // } else{
+                        //     //send repo rata
+                        //     ui.showRepo(data2)
+                        // }
+
+                    })
+                //console.log(data.repos_url);
+
+                if (data.message == 'Not Found') {
+                    //Show Alert that usernamen not found
+                    ui.showAlert("User not found!", "alert alert-danger")
+                } else {
+                    //send profile data
+                    ui.showProfile(data)
+                }
+            })
+
+    } else {
+        //Clear Profile
+        ui.clearProfile();
+    }
+})
+
