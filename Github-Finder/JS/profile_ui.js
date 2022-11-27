e5aecaaab434bed3c267b16fdbfe455ffc0dee7f
Ferdaus Zaman Polok <ferdaus.study@gmail.com>
2022-11-27 18:41:19 +0600

    Formated the codes

--- Github-Finder/JS/profile_ui.js
+++ Github-Finder/JS/profile_ui.js
@@ -8,17 +8,22 @@ class UI {
     this.clearAlert(); //Clear if there is a alart showing from last search result
 
     //populate li into a string in case of having public repo
-    if(haveRepo){
-      var str = '<ul>'
+    if (haveRepo) {
+      var str = "<ul>";
       for (var i = 0; i < repo.length; i++) {
-        str += '<li>' + repo[i].name + '<a target="_blank" href=' + repo[i].html_url +'> <button class="btn btn-outline-dark rounded mt-2 mb-2 btn-sm" >View on GitHub</button>  </a> </li>'
+        str +=
+          "<li>" +
+          repo[i].name +
+          '<a target="_blank" href=' +
+          repo[i].html_url +
+          '> <button class="btn btn-outline-dark rounded mt-2 mb-2 btn-sm" >View on GitHub</button>  </a> </li>';
       }
-      str += '</ul>';
+      str += "</ul>";
     }
-    
+
     //in case of not having public repo
-    else{
-      str= "<h6> This user doen't have any Public Repo. 🙂 </h6>"
+    else {
+      str = "<h6> This user doen't have any Public Repo. 🙂 </h6>";
     }
 
     //populating the main content div #profile
@@ -27,24 +32,50 @@ class UI {
         <div class="row">
         <div class="col-md-3">
             <img class="img-fluid mb-2" src="${profile.avatar_url}">
-            <p target="_blank" class="btn btn-dark btn-block mb-1" style= "border-radius: 4px;" >${profile.name}</p>
-            <p class="text-center list-group-item mb-1" style= "font-size: 14px; font-family: sans-serif; " >${profile.bio} </p>
-            <a href="${profile.html_url}" target="_blank" class="btn btn-primary btn-block mb-1" style= "border-radius: 4px;" >View Profile</a>
+            <p target="_blank" class="btn btn-dark btn-block mb-1" style= "border-radius: 4px;" >${
+              profile.name
+            }</p>
+            <p class="text-center list-group-item mb-1" style= "font-size: 14px; font-family: sans-serif; " >${
+              profile.bio
+            } </p>
+            <a href="${
+              profile.html_url
+            }" target="_blank" class="btn btn-primary btn-block mb-1" style= "border-radius: 4px;" >View Profile</a>
         </div>
           <div class="col-md-9">
-            <span class="badge badge-danger">Public Repos: ${profile.public_repos}</span>
-            <span class="badge badge-info">Followers: ${profile.followers}</span>
-            <span class="badge badge-warning">Following: ${profile.following}</span>
+            <span class="badge badge-danger">Public Repos: ${
+              profile.public_repos
+            }</span>
+            <span class="badge badge-info">Followers: ${
+              profile.followers
+            }</span>
+            <span class="badge badge-warning">Following: ${
+              profile.following
+            }</span>
             <br><br>
             <ul class="list-group">
-            <li class="list-group-item"> <b> profilename: </b> ${profile.login} </li>
+            <li class="list-group-item"> <b> profilename: </b> ${
+              profile.login
+            } </li>
             <li class="list-group-item"> <b> ID: </b> ${profile.id} </li>
             <li class="list-group-item"> <b> Email: </b> ${profile.email} </li>
-            <li class="list-group-item"> <b> Company: </b> ${profile.company} </li>
-            <li class="list-group-item"> <b> Website/Blog: </b> ${profile.blog} </li>
-            <li class="list-group-item"> <b> Location: </b> ${profile.location} </li>
-            <li class="list-group-item"> <b> Member Since: </b> ${profile.created_at.slice(0, 7)} </li> 
-            <li class="list-group-item"> <b> Last Contribution: </b> ${profile.updated_at.slice(0, 7)} </li> 
+            <li class="list-group-item"> <b> Company: </b> ${
+              profile.company
+            } </li>
+            <li class="list-group-item"> <b> Website/Blog: </b> ${
+              profile.blog
+            } </li>
+            <li class="list-group-item"> <b> Location: </b> ${
+              profile.location
+            } </li>
+            <li class="list-group-item"> <b> Member Since: </b> ${profile.created_at.slice(
+              0,
+              7
+            )} </li> 
+            <li class="list-group-item"> <b> Last Contribution: </b> ${profile.updated_at.slice(
+              0,
+              7
+            )} </li> 
             </ul>
           </div>
         </div>
@@ -86,5 +117,3 @@ class UI {
     }
   }
 }
-
-
--- Github-Finder/JS/script.js
+++ Github-Finder/JS/script.js
@@ -2,47 +2,46 @@ let searchBtn = document.querySelector("#searchBtn"); //search button
 let searchUser = document.querySelector("#searchUser"); //user input form data from search bar
 let ui = new UI();
 
-searchBtn.addEventListener("click", fetch_data) //for button click
-searchUser.addEventListener("keyup", e =>{ //for enter key
-    if (e.keyCode ===13){
-        fetch_data();
-    }
-})
+searchBtn.addEventListener("click", fetch_data); //for button click
+searchUser.addEventListener("keyup", (e) => {
+  //for enter key
+  if (e.keyCode === 13) {
+    fetch_data();
+  }
+});
 
-function fetch_data(){
-    let userGivenText = searchUser.value;
-    if (userGivenText != "") {
-        //Geting User Data via Fetch API
-        fetch(`https://api.github.com/users/${userGivenText}`)
-            .then(result => result.json())
-            .then(profile_data => {
-                //Geting User Repo Data via Fetch API   
-                fetch(profile_data.repos_url)
-                    .then(result => result.json())
-                    .then(repo_data => {
+function fetch_data() {
+  let userGivenText = searchUser.value;
+  if (userGivenText != "") {
+    //Geting User Data via Fetch API
+    fetch(`https://api.github.com/users/${userGivenText}`)
+      .then((result) => result.json())
+      .then((profile_data) => {
+        //Geting User Repo Data via Fetch API
+        fetch(profile_data.repos_url)
+          .then((result) => result.json())
+          .then((repo_data) => {
+            console.log(repo_data);
+            if (repo_data == "") {
+              //in case of not having public repo
+              ui.showProfile(profile_data, repo_data, false);
+            } else {
+              //in case of having public repo
+              ui.showProfile(profile_data, repo_data, true);
+            }
+          });
+        //console.log(data.repos_url);
 
-                        console.log(repo_data);
-                        if (repo_data == '') { //in case of not having public repo
-                            ui.showProfile(profile_data, repo_data, false)
-                        }
-                        else{ //in case of having public repo
-                            ui.showProfile(profile_data, repo_data, true)
-                        }
-                    })
-                //console.log(data.repos_url);
-
-                if (profile_data.message == 'Not Found') {
-                    //Show Alert that usernamen not found
-                    ui.showAlert("User not found!", "alert alert-danger")
-                } else {
-                    //send profile data
-                    ui.showProfile(profile_data)
-                }
-            })
-
-    } else {
-        //Clear Profile
-        ui.clearProfile();
-    }
+        if (profile_data.message == "Not Found") {
+          //Show Alert that usernamen not found
+          ui.showAlert("User not found!", "alert alert-danger");
+        } else {
+          //send profile data
+          ui.showProfile(profile_data);
+        }
+      });
+  } else {
+    //Clear Profile
+    ui.clearProfile();
+  }
 }
-
