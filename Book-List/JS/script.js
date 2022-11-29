60f7b9a0c562d6e7a558c071775d2fafb124d0e7
Ferdaus Zaman Polok <ferdaus.study@gmail.com>
2022-11-29 18:26:00 +0600

    Alert showing bug fixed

--- Book-List/JS/script.js
+++ Book-List/JS/script.js
@@ -1,13 +1,13 @@
 //Getting the UI elements
 let form = document.querySelector('#book-form');
-let bookList= document.querySelector('#book-list');
+let bookList = document.querySelector('#book-list');
 
 //Book Class
-class Book{
-    constructor(title, author, isbn){
-        this.title= title;
+class Book {
+    constructor(title, author, isbn) {
+        this.title = title;
         this.author = author;
-        this.isbn= isbn
+        this.isbn = isbn
     }
 }
 
@@ -16,17 +16,17 @@ form.addEventListener('submit', newBook);
 bookList.addEventListener('click', removeBook);
 
 //Writting functions
-function newBook(e){
+function newBook(e) {
     // console.log("submitted")
-    let title= document.querySelector("#title").value;
-    let author= document.querySelector("#author").value;
-    let isbn= document.querySelector("#isbn").value;
+    let title = document.querySelector("#title").value;
+    let author = document.querySelector("#author").value;
+    let isbn = document.querySelector("#isbn").value;
 
-    if(title == "" || author == "" || isbn == "" ){
+    if (title == "" || author == "" || isbn == "") {
         UI.showalert("Please Fill up all the boxes bellow & try again!", "error")
     }
 
-    else{
+    else {
         let book = new Book(title, author, isbn);
         //console.log(book)
         UI.addBookList(book)
@@ -36,36 +36,38 @@ function newBook(e){
     }
 }
 
-function removeBook(e){
-    UI.deleteFromBook(e.target);
-    UI.showalert("Book Removed!", "success");
+function removeBook(e) {
+    if (e.target.hasAttribute('href')) {
+        UI.deleteFromBook(e.target);
+        UI.showalert("Book Removed!", "success");
+    }
     e.preventDefault();
 }
 
 //UI Class
-class UI{ //To Add books in the table data
+class UI { //To Add books in the table data
 
-    static addBookList(book){
+    static addBookList(book) {
         //console.log(book)
         let list = document.querySelector("#book-list");
         let row = document.createElement('tr');
-        row.innerHTML= `
+        row.innerHTML = `
         <td> ${book.title} </td>
         <td> ${book.author} </td>
         <td> ${book.isbn} </td>
         <td> <a href = '#' class='delete' style="color: red; text-align: center; text-decoration:none;" >x</td>
         `
         list.appendChild(row)
-        
+
     }
 
-    static clearFields(){
-        let title= document.querySelector("#title").value = "";
-        let author= document.querySelector("#author").value = "";
-        let isbn= document.querySelector("#isbn").value = "";
+    static clearFields() {
+        let title = document.querySelector("#title").value = "";
+        let author = document.querySelector("#author").value = "";
+        let isbn = document.querySelector("#isbn").value = "";
     }
 
-    static showalert(message, alertClassName){
+    static showalert(message, alertClassName) {
         let div = document.createElement('div');
         div.className = `alert ${alertClassName}`; //will create a div with diff alert class of skeleton css
         div.appendChild(document.createTextNode(message));
@@ -73,16 +75,13 @@ class UI{ //To Add books in the table data
         let form = document.querySelector("#book-form");
         container.insertBefore(div, form); //will add div before form after container
 
-        setTimeout(function(){ //to vanish the alert after 3s
+        setTimeout(function () { //to vanish the alert after 3s
             document.querySelector(".alert").remove();
         }, 3000)
     }
 
-    static deleteFromBook(target){
-        if(target.hasAttribute('href')){
-            target.parentElement.parentElement.remove()
-        }
-        
+    static deleteFromBook(target) {
+        target.parentElement.parentElement.remove()
     }
 
 }
